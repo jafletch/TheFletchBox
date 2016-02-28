002d19c5c91e1a50c3b180c57902d694088871d1
James Fletcher <jafletch@msn.com>
2016-02-28 14:39:53 -0800

    Try out multiple inheritance in devices

--- Makeblock/RaspberryPiShield/devices.py
+++ Makeblock/RaspberryPiShield/devices.py
@@ -29,57 +29,53 @@ class slotteddevice(simpledevice):
         super(slotteddevice, self).__init__(deviceType)
         self.slot = slot.validate(moduleSlot)
 
-class temperatureSensor(slotteddevice):
+class readabledevice():
 
-    def __init__(self, moduleSlot):
-        super(temperatureSensor, self).__init__(device.TEMPERATURE_SENSOR, moduleSlot)
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
         self.__value = -1
 
-    def requestTemp(self):
+    def requestValue(self):
         self.port.sendRequest(requestpacket(self.index, action.GET, self.device, self.port.id, self.slot))
 
     def latestValue(self):
         return self.__value
+
+    @abstractmethod
+    def parseData(self, data):
+        return None
+
+class temperatureSensor(slotteddevice, readabledevice):
+
+    def __init__(self, moduleSlot):
+        super(temperatureSensor, self).__init__(device.TEMPERATURE_SENSOR, moduleSlot)
 
     def parseData(self, data):
         if len(data) != 4:
             raise PacketError("Expected 4 bytes of data returned. Got: " + str(len(data)))
         self.__value = self.bytesToFloat(data, 0)
 
-class lightAndGrayscaleSensor(simpledevice):
+class lightAndGrayscaleSensor(simpledevice, readabledevice):
 
     def __init__(self):
         super(lightAndGrayscaleSensor, self).__init__(device.LIGHT_SENSOR)
-        self.__value = -1
-
-    def requestValue(self):
-        self.port.sendRequest(requestpacket(self.index, action.GET, self.device, self.port.id))
-
+        
     def lightOn(self):
         self.port.sendRequest(requestpacket(self.index, action.RUN, self.device, self.port.id, data=[1]))
 
     def lightOff(self):
         self.port.sendRequest(requestpacket(self.index, action.RUN, self.device, self.port.id, data=[0]))
 
-    def latestValue(self):
-        return self.__value
-
     def parseData(self, data):
         if len(data) != 4:
             raise PacketError("Expected 4 bytes of data returned. Got: " + str(len(data)))
         self.__value = self.bytesToFloat(data, 0)
 
-class ultrasonicSensor(simpledevice):
+class ultrasonicSensor(simpledevice, readabledevice):
 
     def __init__(self):
         super(ultrasonicSensor, self).__init__(device.ULTRASONIC_SENSOR)
-        self.__value = -1
-
-    def requestValue(self):
-        self.port.sendRequest(requestpacket(self.index, action.GET, self.device, self.port.id))
-
-    def latestValue(self):
-        return self.__value
 
     def parseData(self, data):
         if len(data) != 4:
