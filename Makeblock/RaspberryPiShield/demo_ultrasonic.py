a763a683056adaeff93b6de55abb3b9ebbbc8e10
James Fletcher <jafletch@msn.com>
2016-02-29 22:21:09 -0800

    Delete unused files

--- Makeblock/RaspberryPiShield/demo_ultrasonic.py
+++
@@ -1,7 +0,0 @@
-
-from rpi_serial import *
-
-while True:
-	time.sleep(1)
-	distance = doTemperature("Port4", "Slot2")
-	print "distance",distance
--- Makeblock/RaspberryPiShield/packetbuilder.py
+++
@@ -1,4 +0,0 @@
-class packetbuilder(object):
-
-    def __init__(self):
-        self.fred = "bob"
